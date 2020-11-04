14a5cbe33a02901f7874ca979dc3c158d6160d8f
Aleksej579 <aleksej.eremenko579@gmail.com>
2020-11-05 00:09:37 +0200

    done

--- src/app/page-auth/page-auth.component.ts
+++ src/app/page-auth/page-auth.component.ts
@@ -1,6 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
 import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2mQCpMJncIzeZNC4uqmR0Q7ny_MEdUMo",
+  authDomain: "authentication-5bff9.firebaseapp.com",
+  databaseURL: "https://authentication-5bff9.firebaseio.com",
+  projectId: "authentication-5bff9",
+  storageBucket: "authentication-5bff9.appspot.com",
+  messagingSenderId: "1007884366914",
+  appId: "1:1007884366914:web:4a857f65569dff161e29db"
+};
+
+export default (!firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app());
 
 @Component({
   selector: 'app-page-auth',
@@ -8,39 +23,45 @@ import firebase from "firebase/app";
   styleUrls: ['./page-auth.component.sass']
 })
 export class PageAuthComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
 
-  //new
   loginForm: any = {
     email: "",
     password: ""
   };
+
   //enter
   entForm() {
-    let email = this.loginForm.email;
-    let password = this.loginForm.password;
+    let email = this.loginForm.eemail;
+    let password = this.loginForm.epassword;
     console.log(`E: ${email} P: ${password}`);
-
+    //exit user
+    firebase.auth().signOut()
+    //enter user
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
       .catch(function (error) {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // ...
+        // console.log('stay this page');
+        window.location.pathname = "/page-auth";
+        if (errorCode) {
+          console.log("Invalid email or password !");
+        }
       });
+
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        console.log(
+          `Congratulation Authorized user: ${user.email
+          } ! You accese to this page`
+        );
+        window.location.pathname = "/page-target";
+      }
+    });
   }
 
-
-
-
-
 }
-
-
-// window.location = '/page-target'
--- src/app/page-reg/page-reg.component.ts
+++ src/app/page-reg/page-reg.component.ts
@@ -2,6 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 import firebase from "firebase/app";
 
+//new
+import "firebase/firestore"; //?
+import "firebase/auth";
+
+//new
+const firebaseConfig = {
+  apiKey: "AIzaSyC2mQCpMJncIzeZNC4uqmR0Q7ny_MEdUMo",
+  authDomain: "authentication-5bff9.firebaseapp.com",
+  databaseURL: "https://authentication-5bff9.firebaseio.com",
+  projectId: "authentication-5bff9",
+  storageBucket: "authentication-5bff9.appspot.com",
+  messagingSenderId: "1007884366914",
+  appId: "1:1007884366914:web:4a857f65569dff161e29db"
+};
+
+//new
+export default (!firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app());
+
+
+
+//old
 @Component({
   selector: 'app-page-reg',
   templateUrl: './page-reg.component.html',
@@ -19,21 +42,35 @@ export class PageRegComponent implements OnInit {
     email: "",
     password: ""
   };
+
   //registr
   regForm() {
-    let email = this.loginForm.email;
-    let password = this.loginForm.password;
+    let email = this.loginForm.cemail;
+    let password = this.loginForm.cpassword;
     console.log(`E: ${email} P: ${password}`);
-
+    //exit user
+    firebase.auth().signOut()
+    //reg user
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .catch(function (error) {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // ...
+        if (errorCode) {
+          console.log("Invalid email or password !");
+        }
       });
+
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        console.log(
+          `Congratulation Register & Authorized user: ${user.email
+          } ! You accese to this page`
+        );
+        window.location.pathname = "/page-target";
+      }
+    });
   }
 
 }
